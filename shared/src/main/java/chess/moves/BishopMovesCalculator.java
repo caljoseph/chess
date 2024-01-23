064fc1cdc93f49a049e072da5050e8f41a61d693
caljoseph <therealcalebbradshaw@gmail.com>
2024-01-22 18:26:51 -0700

    fix logic error in bishop

--- shared/src/main/java/chess/moves/BishopMovesCalculator.java
+++ shared/src/main/java/chess/moves/BishopMovesCalculator.java
@@ -6,27 +6,32 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class BishopMovesCalculator implements PieceMovesCalculator {
+    HashSet<ChessMove> moves = new HashSet<ChessMove>();
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        //add diagonal moves in each direction until we hit an edge or another piece.
-        addDiagonals(board, myPosition, 1, 1);
-        addDiagonals(board, myPosition, 1, -1);
-        addDiagonals(board, myPosition, -1, 1);
-        addDiagonals(board, myPosition, -1, -1);
+        // Implement logic for moves
+        addDiagonals(board, myPosition, "NE");
+        addDiagonals(board, myPosition, "NW");
+        addDiagonals(board, myPosition, "SE");
+        addDiagonals(board, myPosition, "SW");
         return moves;
     }
-    HashSet<ChessMove> moves = new HashSet<ChessMove>();
-    void addDiagonals(ChessBoard board, ChessPosition startPosition, int rowDirection, int colDirection) {
+    void addDiagonals(ChessBoard board, ChessPosition startPosition, String direction) {
         ChessPosition currentPosition = startPosition;
-        while (1 < currentPosition.getRow() && currentPosition.getRow() < 8
-                && 1 < currentPosition.getColumn() && currentPosition.getColumn() < 8 ) {
-            currentPosition = getNextPosition(currentPosition, rowDirection, colDirection);
-            if (board.getPiece(currentPosition) == null){
+        while (0 < currentPosition.getRow() && currentPosition.getRow() < 8
+                && 0 < currentPosition.getColumn() && currentPosition.getColumn() < 8 ) {
+            if (getNextPosition(currentPosition, direction) == null){
+                //if we are going to fall off of the board, try another direction
+                break;
+            }
+            if (board.getPiece(getNextPosition(currentPosition, direction)) == null){
+                currentPosition = getNextPosition(currentPosition, direction);
                 ChessMove currentMove = new ChessMove(startPosition, currentPosition, null);
                 moves.add(currentMove);
                 //adds empty squares
             }
-            else if(!board.getPiece(currentPosition).getTeamColor().equals(board.getPiece(startPosition).getTeamColor())) {
+            else if(!board.getPiece(getNextPosition(currentPosition, direction)).getTeamColor().equals(board.getPiece(startPosition).getTeamColor())) {
+                currentPosition = getNextPosition(currentPosition, direction);
                 ChessMove currentMove = new ChessMove(startPosition, currentPosition, null);
                 moves.add(currentMove);
                 break;
@@ -39,8 +44,39 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
 
         }
     }
-    ChessPosition getNextPosition(ChessPosition currentPosition, int rowDirection, int colDirection){
-        return new ChessPosition(currentPosition.getRow() + rowDirection, currentPosition.getColumn() + colDirection);
+    ChessPosition getNextPosition(ChessPosition currentPosition, String direction) {
+        return switch (direction) {
+            case "NE" -> getNextPositionNE(currentPosition);
+            case "NW" -> getNextPositionNW(currentPosition);
+            case "SE" -> getNextPositionSE(currentPosition);
+            case "SW" -> getNextPositionSW(currentPosition);
+            default -> null;
+        };
+    }
+
+    ChessPosition getNextPositionNE(ChessPosition currentPosition) {
+        if(currentPosition.getRow() == 8 || currentPosition.getColumn() == 8) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow() + 1, currentPosition.getColumn() + 1);
+    }
+    ChessPosition getNextPositionNW(ChessPosition currentPosition) {
+        if(currentPosition.getColumn() == 1 || currentPosition.getRow() == 8) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow() + 1, currentPosition.getColumn() - 1);
+    }
+    ChessPosition getNextPositionSE(ChessPosition currentPosition) {
+        if(currentPosition.getColumn() == 8 || currentPosition.getRow() == 1) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow() - 1, currentPosition.getColumn() + 1);
+    }
+    ChessPosition getNextPositionSW(ChessPosition currentPosition) {
+        if(currentPosition.getRow() == 1 || currentPosition.getColumn() == 1) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow() - 1, currentPosition.getColumn() - 1);
     }
 }
 
--- shared/src/main/java/chess/moves/RookMovesCalculator.java
+++ shared/src/main/java/chess/moves/RookMovesCalculator.java
@@ -8,9 +8,78 @@ import java.util.Collection;
 import java.util.HashSet;
 
 public class RookMovesCalculator implements PieceMovesCalculator {
+    HashSet<ChessMove> moves = new HashSet<ChessMove>();
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         // Implement logic for moves
-        // Add valid moves to the collection
-        return new HashSet<>();    }
+        addOrthogonals(board, myPosition, "up");
+        addOrthogonals(board, myPosition, "left");
+        addOrthogonals(board, myPosition, "right");
+        addOrthogonals(board, myPosition, "down");
+        return moves;
+    }
+    void addOrthogonals(ChessBoard board, ChessPosition startPosition, String direction) {
+        ChessPosition currentPosition = startPosition;
+        while (0 < currentPosition.getRow() && currentPosition.getRow() < 8
+                && 0 < currentPosition.getColumn() && currentPosition.getColumn() < 8 ) {
+            if (getNextPosition(currentPosition, direction) == null){
+                //if we are going to fall off of the board, try another direction
+                break;
+            }
+            if (board.getPiece(getNextPosition(currentPosition, direction)) == null){
+                currentPosition = getNextPosition(currentPosition, direction);
+                ChessMove currentMove = new ChessMove(startPosition, currentPosition, null);
+                moves.add(currentMove);
+                //adds empty squares
+            }
+            else if(!board.getPiece(getNextPosition(currentPosition, direction)).getTeamColor().equals(board.getPiece(startPosition).getTeamColor())) {
+                currentPosition = getNextPosition(currentPosition, direction);
+                ChessMove currentMove = new ChessMove(startPosition, currentPosition, null);
+                moves.add(currentMove);
+                break;
+                //stops at capture
+            }
+            else {
+                break;
+                //stops and does not permit move on encounter with teammate
+            }
+
+        }
+    }
+    ChessPosition getNextPosition(ChessPosition currentPosition, String direction) {
+        return switch (direction) {
+            case "up" -> getNextPositionUp(currentPosition);
+            case "left" -> getNextPositionLeft(currentPosition);
+            case "right" -> getNextPositionRight(currentPosition);
+            case "down" -> getNextPositionDown(currentPosition);
+            default -> null;
+        };
+    }
+
+    ChessPosition getNextPositionUp(ChessPosition currentPosition) {
+        if(currentPosition.getRow() == 8) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow() + 1, currentPosition.getColumn());
+    }
+    ChessPosition getNextPositionLeft(ChessPosition currentPosition) {
+        if(currentPosition.getColumn() == 1) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow(), currentPosition.getColumn() - 1);
+    }
+    ChessPosition getNextPositionRight(ChessPosition currentPosition) {
+        if(currentPosition.getColumn() == 8) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow(), currentPosition.getColumn() + 1);
+    }
+    ChessPosition getNextPositionDown(ChessPosition currentPosition) {
+        if(currentPosition.getRow() == 1) {
+            return null;
+        }
+        return new ChessPosition(currentPosition.getRow() - 1, currentPosition.getColumn());
+    }
 }
+
+
